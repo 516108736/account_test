4269ff1ff53e93e3072c907b9e1416ed1c9c3bbf
516108736 <516108736@qq.com>
2020-08-13 20:00:41 +0800

    modify sh

--- cmd/full_test.sh
+++ cmd/full_test.sh
@@ -1,30 +1,29 @@
-rm -rf *.log fastdb_data iavl_data mpt_data
+rm -rf *.log *data
 echo 3 > /proc/sys/vm/drop_caches
 go run main.go --typ=mpt    --initNumber=1000000 >> test_mpt_100w.log
 go run main.go --typ=iavl   --initNumber=1000000 >> test_iavl_100w.log
 go run main.go --typ=fastdb --initNumber=1000000 >> test_fastdb_100w.log
 go run main.go --typ=mpts   --initNumber=1000000 >> test_mpts_100w.log
-zip -r 100w.zip *.log fastdb_data iavl_data mpt_data mpts_data
+zip -r 100w.zip *.log *_data
 
 
 
 
-rm -rf  fastdb_data iavl_data mpt_data
+rm -rf  *_data
 echo 3 > /proc/sys/vm/drop_caches
 go run main.go --typ=mpt    --initNumber=10000000 >> test_mpt_1000w.log
 go run main.go --typ=iavl   --initNumber=10000000 >> test_iavl_1000w.log
 go run main.go --typ=fastdb --initNumber=10000000 >> test_fastdb_1000w.log
 go run main.go --typ=mpts   --initNumber=10000000 >> test_mpts_1000w.log
-zip -r 1000w.zip *.log fastdb_data iavl_data mpt_data mpts_data
+zip -r 1000w.zip *.log *_data
 
 
 
 
-
-rm -rf  fastdb_data iavl_data mpt_data
+rm -rf  *_data
 echo 3 > /proc/sys/vm/drop_caches
 go run main.go --typ=mpt    --initNumber=100000000 >> test_mpt_10000w.log
 go run main.go --typ=iavl   --initNumber=100000000 >> test_iavl_10000w.log
 go run main.go --typ=fastdb --initNumber=100000000 >> test_fastdb_10000w.log
 go run main.go --typ=mpts   --initNumber=100000000 >> test_mpts_10000w.log
-zip -r 10000w.zip *.log fastdb_data iavl_data mpt_data mpts_data
+zip -r 10000w.zip *.log *_data
